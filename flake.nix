5f9e89eeccd92321ae0231e3581cbcb77aaf3d5e
Robbin Bonthond <robbin@bonthond.com>
2022-11-15 12:23:40 -0600

    updated

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.url = github:cachix/devenv/v0.1;
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager }: let
+  outputs = inputs @ { self, nixpkgs, home-manager, devenv }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
